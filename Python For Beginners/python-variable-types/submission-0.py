ade129d606102df17f9f632bb3b732a42ce0995d
Abhay Singh <161878933+ASR222@users.noreply.github.com>
2026-08-13 16:25:48 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 3
+float_type = 3.6
+bool_type = True
+string_type = 'abhay'
+list_type = [2,3,4,45]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
