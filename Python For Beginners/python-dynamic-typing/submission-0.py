742e95ea2a1749b133ba92f8472cff3b1cd12c58
Abhay Singh <161878933+ASR222@users.noreply.github.com>
2026-08-13 16:28:02 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 4
+print(type(variable))
+variable = 4.6
+print(type(variable))
+variable = False
+print(type(variable))
+variable = 'string'
+print(type(variable))
+variable = ['ab', 'cd', 'ef']
+print(type(variable))
